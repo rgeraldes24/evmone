3cd9f66442c2a01d20a12d3eb17f665c9d308f5f
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-10-12 20:24:16 +0400

    Review marked items

--- test/state/mpt_hash.cpp
+++ test/state/mpt_hash.cpp
@@ -38,8 +38,16 @@ hash256 mpt_hash(const std::unordered_map<address, Account>& accounts)
 hash256 mpt_hash(std::span<const Transaction> transactions)
 {
     MPT trie;
-    for (size_t i = 0; i < transactions.size(); ++i)
+    for (size_t i = 0; i < transactions.size(); ++i) {
+        // EXPECT_EQ(rlp::encode(transactions[i])_hex),
+        // 0x167da6f024b270d25a7ca05c1bf373c9636517b16a4b5f7a9fb975a668dc00e9_bytes32);
+        // printf("AQUI\n");
+        // printf("%x\n", rlp::encode(i)_hex);
+        // printf("%x\n", rlp::encode(transactions[i])_hex);
+        // printf("AQUI_END\n");
         trie.insert(rlp::encode(i), rlp::encode(transactions[i]));
+    }
+        
 
     return trie.hash();
 }
